8681f95938a366adb34eb48b7c5b2849682ef688
Luis Guilherme De Souza Silva <luis.guilherme@engdb.com.br>
2018-09-17 21:29:57 -0300

    Primeiro commit.

---
+++ src/main/java/com/techipster/siscomercial/SiscomercialApplication.java
@@ -0,0 +1,12 @@
+package com.techipster.siscomercial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiscomercialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiscomercialApplication.class, args);
+	}
+}
